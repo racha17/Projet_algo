bd9e79e3c44491fa8c697f01a5cac675b73eed42
racha17 <benaissaracha160604@gmail.com>
2023-12-22 17:21:43 +0100

    corrected some things

--- main.c
+++ main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct list list struct list
+typedef struct list
 {
     int nbr;
     struct list *svt;
-};
-typedef struct pile pile struct pile
+} list;
+
+typedef struct Pile
 {
     list *first;
-};
+} Pile;
 
-void empiler(pile *pile, int val)
+void empiler(Pile *pile, int val)
 {
     list *L = malloc(sizeof(*L));
     if (pile == NULL || L == NULL)
@@ -24,6 +25,37 @@ void empiler(pile *pile, int val)
     pile->first = L;
 }
 
+void afficher(Pile *pile)
+{
+    if (pile == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+    list *P = pile->first;
+    while (P != NULL)
+    {
+        printf("%d\n", P->nbr);
+        P = P->svt;
+    }
+    printf("\n");
+}
+
 int main()
 {
+
+    Pile Mapile;
+    Mapile.first = NULL;
+    int N, val;
+    printf("entrez le max de valeurs que peut contenir la pile:\n");
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &val);
+        empiler(&Mapile, val);
+    }
+     printf("les valeurs de la pile sont:\n");
+    afficher(&Mapile);
+
+    return 0;
 }
