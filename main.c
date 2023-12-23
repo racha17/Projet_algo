071571f5607e4ded2623745851f91d99c0b8cf80
racha17 <benaissaracha160604@gmail.com>
2023-12-23 23:00:17 +0100

    added predefined functions and corrected some functions

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct list
 {
@@ -7,15 +8,36 @@ typedef struct list
     struct list *svt;
 } list;
 
-typedef struct Pile
+typedef struct Pile{
+list *first;
+}Pile;
+
+Pile initPile()
 {
-    list *first;
-} Pile;
+    Pile *p=malloc(sizeof(Pile));
+    p->first=NULL;
+    return *p  ;
+}
+
+bool Pilevide(Pile *pile)
+{
+    return pile->first==NULL;
+}
+
+int SommetPile(Pile *pile)
+{
+    if(Pilevide(pile)){
+        exit(EXIT_FAILURE);
+    }
+    return pile->first->nbr;
+
+}
 
 void empiler(Pile *pile, int val)
 {
-    list *L = malloc(sizeof(*L));
-    if (pile == NULL || L == NULL)
+
+    list *L = malloc(sizeof(list));
+    if (L==NULL)
     {
         exit(EXIT_FAILURE);
     }
@@ -25,37 +47,65 @@ void empiler(Pile *pile, int val)
     pile->first = L;
 }
 
-void afficher(Pile *pile)
+int depiler(Pile *pile)
 {
-    if (pile == NULL)
+    int nbrdepile = 0;
+    list *L = pile->first;
+    if (Pilevide(pile))
     {
         exit(EXIT_FAILURE);
     }
-    list *P = pile->first;
-    while (P != NULL)
+
+    if (L != NULL)
     {
-        printf("%d\n", P->nbr);
-        P = P->svt;
+        nbrdepile = L->nbr;
+        pile->first = L->svt;
+        free(L);
     }
+    return nbrdepile;
+}
+
+void afficher(Pile *pile)
+{
+    int val;
+    if (Pilevide(pile))
+    {
+        printf("la pile est vide\n");
+        exit(EXIT_FAILURE);
+    }
+
+    Pile pile1=initPile();
+
+    while(!Pilevide(pile)){
+        val=depiler(pile);
+        printf("%d\n",val);
+        empiler(&pile1,val);
+    }
+    while(!Pilevide(&pile1)){
+        empiler(pile,depiler(&pile1));
+    }
+
     printf("\n");
 }
 
 int main()
 {
 
-    Pile Mapile;
-    Mapile.first = NULL;
-    int N, val;
+    Pile Mapile=initPile();
+    int N,val;
+
     printf("entrez le max de valeurs que peut contenir la pile:\n");
     scanf("%d", &N);
+    printf("%d\n",N);
 
     for (int i = 0; i < N; i++)
     {
         scanf("%d", &val);
         empiler(&Mapile, val);
     }
-     printf("les valeurs de la pile sont:\n");
+    printf("les valeurs de la pile sont:\n");
     afficher(&Mapile);
-
     return 0;
 }
+
+
