7a77753313c351b8350cf52c0399f9fb1fbd304d
sanaa-ber <154596981+sanaa-ber@users.noreply.github.com>
2023-12-29 00:29:38 +0100

    Update F_predefenies.h

--- F_predefenies.h
+++ F_predefenies.h
@@ -9,7 +9,14 @@ int SommetPile(Pile *pile);
 void empiler(Pile *pile, int val);
 int depiler(Pile *pile);
 void afficher(Pile *pile);
-bool recherche(Pile *pile, int val);
+
+/* pour l'algorithme de transformation */
+void empiler(Pile *pile, char element);
+int pilePleine(Pile *pile);
+char depiler(Pile *pile);
+char sommetPile(Pile *pile);
+int pileVide2(Pile *pile);
+void initPile2(Pile *pile);
 
 
 #endif // F_PREDEFINIES_H_INCLUDED
