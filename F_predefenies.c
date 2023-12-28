6b2e1858515db1e27f32f96366f376542cf9fbe4
sanaa-ber <154596981+sanaa-ber@users.noreply.github.com>
2023-12-28 23:54:59 +0100

    Update F_predefenies.c

--- F_predefenies.c
+++ F_predefenies.c
@@ -56,3 +56,28 @@ int depiler(Pile *pile)
     }
     return nbrdepile;
 }
+
+bool recherche(Pile *pile, int val)
+{
+    bool trouve;
+
+    Pile R = initPile();
+    trouve = false;
+
+    while (!Pilevide(pile) && SommetPile(pile) != val)
+    {
+        empiler(&R, depiler(pile));
+    }
+
+    if (!Pilevide(pile))
+    {
+        trouve = true;
+    }
+
+    while (!Pilevide(&R))
+    {
+        empiler(pile, depiler(&R));
+    }
+
+    return trouve;
+}
