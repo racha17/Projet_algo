cab669136b28922859654ab565d8179145fb30cf
sanaa-ber <154596981+sanaa-ber@users.noreply.github.com>
2024-01-02 22:23:21 +0100

    Update Fonctions.c

--- include/Fonctions.c
+++ include/Fonctions.c
@@ -58,3 +58,27 @@ void insertion(Pile *pile, int val)
 {
     empiler(pile, val);
 }
+bool recherche(Pile *pile, int val)
+{
+    bool trouve;
+
+    Pile R = initPile();
+    trouve = false;
+
+    while (!Pilevide(pile) && SommetPile(pile) != val)
+    {
+        empiler(&R, depiler(pile));
+    }
+
+    if (!Pilevide(pile))
+    {
+        trouve = true;
+    }
+
+    while (!Pilevide(&R))
+    {
+        empiler(pile, depiler(&R));
+    }
+
+    return trouve;
+}
