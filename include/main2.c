1a12b8560783097bcdf8d52bb712b0376a741819
Racha <116683255+racha17@users.noreply.github.com>
2024-01-06 22:14:46 +0100

    Update main2.c

--- include/main2.c
+++ include/main2.c
@@ -6,7 +6,61 @@
 #include "structure.h"
 #include "F_predefinies.h"
 #include "Fonctions.h"
+int main{
+  // Initialiser la fenêtre
+    // Pile Mapile = initPile();
+    //  int N, val , valeur , val2;
+    //    char choix;
+    // printf("entrez le max de valeurs que peut contenir la pile:\n");
+    // scanf("%d", &N);
 
+    // for (int i = 0; i < N; i++)
+    // {
+    //     scanf("%d", &val);
+    //     empiler(&Mapile, val);
+    // }
+    // printf("les valeurs de la pile sont:\n");
+    // afficher(&Mapile);
+
+    // printf("donnez la valeur que vous voulez supprimer:\n");
+    // scanf("%d", &val);
+    // suppression(&Mapile, val);
+    // printf("l'etat de la pile apres suppression:\n");
+    // afficher(&Mapile);
+
+    //       // test INSERTION
+    //     printf ("voulez-vous entrer une valeur O/N ?:\n");
+    //     scanf ("%c",&choix);
+    //     if (choix=='O'||choix=='o'){
+    //         /*demander à l'utilisateur d'entrer une valeur*/
+    //     printf ("donnez une valeur à ajouter:\n");
+    //     scanf ("%d",&valeur);
+    //     } else {
+    //         /*choisir une valeur aléatoire*/
+
+    //         valeur = rand() %100; /*valeurs entre 0 et 99*/
+    //     }
+    //     /*inserer la valeur dans la pile*/
+    //     insertion(&Mapile,valeur);
+    //     /*affichage*/
+    //     printf ("Etat de la pile aprés insertion :\n");
+    //     afficherPile(&Mapile);
+    /* printf("veuillez donner la valeur que vous voulez rechercher ");
+scanf("%d",&val2);
+
+if (recherche(&Mapile,val2)==false)
+{
+    printf("la valeur n'existe pas. \n ");
+
+}
+else printf("la valeur existe . \n");
+
+
+     */
+
+    //   return 0;
+
+    //}
 /* UTILISATION DES PILES DANS
 L’EVALUATION DES EXPRESSIONS
 ARITHMETIQUES */
