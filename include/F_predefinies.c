077d05ccb5404973bc13ece098fe220094c8b097
Malak <154674345+Malak-brt@users.noreply.github.com>
2024-01-10 10:35:27 +0100

    Update F_predefinies.c

--- include/F_predefinies.c
+++ include/F_predefinies.c
@@ -56,3 +56,20 @@ int depiler(Pile *pile)
     }
     return nbrdepile;
 }
+void depiler2(Pile *pile,int x)
+{
+    
+    list *L = pile->first;
+    if (Pilevide(pile))
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    if (L != NULL)
+    {
+        x = L->nbr;
+        pile->first = L->svt;
+        free(L);
+    }
+    
+}
