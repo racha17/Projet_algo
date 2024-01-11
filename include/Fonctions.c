e06b21174e306caf1bf36bfc5d520d627eb50808
Racha <116683255+racha17@users.noreply.github.com>
2024-01-11 20:27:14 +0100

    Update Fonctions.c

--- include/Fonctions.c
+++ include/Fonctions.c
@@ -54,3 +54,105 @@ bool suppression(Pile *pile, int val)
     }
     return trouve;
 }
+void insertion (Pile *pile,int val,int k)
+{    Pile R;
+    int x;
+    if (Pilevide(&pile)){
+    empiler (&pile,val);
+    }
+    else if (k==1){
+    empiler(&pile,val);
+    }
+
+    else {
+        initPile(&R);
+        while (!Pilevide(&pile) && k>1){
+            depiler2(&pile,x);
+            empiler(&R,x);
+            k=k-1;
+        }
+    }
+    if (k==1){
+        empiler(&pile,val);
+    }else, { printf ("erreur!!!");}
+    while (!Pilevide(&pile)){
+            depiler2 (&R,x);
+            empiler (&pile,x);
+        }
+ }
+bool recherche(Pile *pile, int val)
+{
+    bool trouve;
+
+    Pile R = initPile();
+    trouve = false;
+
+    while (!Pilevide(pile) && SommetPile(pile) != val)
+    {
+        empiler(&R, depiler(pile));
+    }
+
+    if (!Pilevide(pile))
+    {
+        trouve = true;
+    }
+
+    while (!Pilevide(&R))
+    {
+        empiler(pile, depiler(&R));
+    }
+
+    return trouve;
+}
+// Algorithme de transformation
+void transformerExpression(char *expression)
+{
+    Pile pileP, pileR;
+    char x;
+
+    initPile(&pileP);
+    initPile(&pileR);
+
+    for (int i = 0; expression[i] != '\0'; i++)
+    {
+        if (estOperande(expression[i]))
+        {
+            empiler(&pileR, expression[i]);
+        }
+        else if (expression[i] == '(')
+        {
+            empiler(&pileP, expression[i]);
+        }
+        else if (estOperateur(expression[i]))
+        {
+            while (!pileVide(&pileP) && estOperateur(sommetPile(&pileP)) &&
+                   (priorite(expression[i]) <= priorite(sommetPile(&pileP))))
+            {
+                x = depiler(&pileP);
+                empiler(&pileR, x);
+            }
+            empiler(&pileP, expression[i]);
+        }
+        else if (expression[i] == ')')
+        {
+            while (!pileVide(&pileP) && sommetPile(&pileP) != '(')
+            {
+                x = depiler(&pileP);
+                empiler(&pileR, x);
+            }
+            depiler(&pileP); // Dépiler la parenthèse ouvrante
+        }
+    }
+
+    while (!pileVide(&pileR))
+    {
+        x = depiler(&pileR);
+        empiler(&pileP, x);
+    }
+
+    while (!pileVide(&pileP))
+    {
+        x = depiler(&pileP);
+        printf("%c ", x);
+    }
+}
